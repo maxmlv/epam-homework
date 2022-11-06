5a387eceea3d30bf82e0e441e7d270988482091e
Maksym Malieiev <maxmlv@maxmlv.localdomain>
2022-11-06 18:12:01 +0200

    now script passing arguments, added comments

--- bash/task3/backup.sh
+++ bash/task3/backup.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-source="source"
-dest="backup/$source"
+# directory to backup
+source="$1"
+# path where backup will be stored
+dest="$2/$(basename $source)"
 latest_backup="${dest}_latest"
-log_file="backup.log"
+log_file="$3"
 
+# Main fucntion that backups 2 replicas of source directory.
+# Latest backup will show the changes between the source,
+# second replica will contain source on that time.
 backup() {
     rsync -ai --delete $source $latest_backup
     rsync -a --delete $source "${dest}_$(date "+%d_%m_%Y_%H_%M")"
 }
 
+# Returns dictionary of directory/file as key with value of operation (create, update, delete)
 changes_dict() {
+    # rsync -i gives a string of all files/directories paired with operation.
+    # It might look like "file1.txt >f+++++++++ folder cd+++++++++"
+    # In this function this string already splited to array and coming through argument
     raw_array=("$@")
     len=${#raw_array[@]}
     echo "("
@@ -23,12 +32,20 @@ changes_dict() {
 }
 
 log_backup() {
+    # Using --dry-run to get output of changes to log
     dry_run=$( rsync -ai --dry-run --delete $source $latest_backup )
     string=$( echo $dry_run )
+    # Spliting dry-run output to array
     IFS=' ' read -r -a array <<< "$string"
+    # Converting array into dictionary
     declare -A changes=$( changes_dict "${array[@]}" )
-    for key in ${!changes[@]}; do
-        key_date=$(date -r $key "+%d-%m-%Y %H:%M:%S")
+    for key in ${!changes[@]}; do 
+        if [[ -z $key ]]; then
+            key_date=$(date -r $key "+%d-%m-%Y %H:%M:%S")
+        else
+            key_date=$(date "+%d-%m-%Y %H:%M:%S")
+        fi
+        # To log all changes implement switch-case
         case ${changes[$key]} in
             "cd+++++++++")
                 echo "[$key_date] <CREATED> directory $key" >> $log_file
